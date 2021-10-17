17e9ec53ba122020c328cd7a7ec48d693aa01322
hanjiyeon <jiyeon53885@gmail.com>
2021-10-17 12:16:10 +0900

    server setup

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log("Lisition on http//localhost:3000");
+app.listen(3000, handleListen);
