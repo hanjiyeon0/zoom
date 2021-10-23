9105a40ff9e1698257f2be457c34d146fddbb4aa
hanjiyeon <jiyeon53885@gmail.com>
2021-10-23 14:28:49 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 
